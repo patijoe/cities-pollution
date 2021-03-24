5b4754e2e1c9dfae48eb477da3e040b11b0b7c0c
patricia.garcia <patricia.garcia@coches.com>
2021-03-24 19:31:20 +0100

    create express server

--- src/App.js
+++ src/App.js
@@ -1,25 +1,46 @@
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+class App extends Component {
+  constructor(props) {
+    super(props);
 
+    this.state = {
+      apiResponse: ''
+    }
+  }
+
+  callAPI() {
+    fetch('http://localhost:9000/cities')
+      .then(response => response.json())
+      .then(response => this.setState({
+        apiResponse: response
+      }))
+  };
+
+  componentDidMount() {
+    this.callAPI();
+  }
+
+  render() {
+    const {apiResponse} = this.state;
+    console.log('---------', apiResponse);
+
+    return (
+      <div className="App">
+        {apiResponse && apiResponse.map(item => {
+          return(
+            <div key={item.id}>
+              <p>{item.id}</p>
+              <p>{item.name}</p>
+            </div>     
+          );}
+        )}
+      </div>
+    );
+  }
+}
+    
+    
 export default App;
