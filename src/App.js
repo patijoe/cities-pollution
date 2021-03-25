10fd1c7ab560dbb3bea683f20286ad660a8b1f0c
patricia.garcia <patricia.garcia@coches.com>
2021-03-25 07:03:52 +0100

    add styled components

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import "./App.css";
+import styled from "styled-components";
 
 export default function App() {
   const [citiesList, setCitiesList] = useState("");
@@ -13,7 +13,7 @@ export default function App() {
   };
 
   return (
-    <div className="App">
+    <AppSection>
       {citiesList &&
         citiesList.map((city) => {
           return (
@@ -23,6 +23,11 @@ export default function App() {
             </div>
           );
         })}
-    </div>
+    </AppSection>
   );
 }
+
+const AppSection = styled.div`
+  text-align: center;
+  color: red;
+`;
